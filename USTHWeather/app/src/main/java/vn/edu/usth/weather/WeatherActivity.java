06f85a9c9a444f8aa5b4aaacc7a5ed59feee916e
Dung Nguyen <dung73432@gmail.com>
2024-09-06 01:54:06 +0700

    Some updates and initial code of practical 5

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,8 +1,14 @@
 package vn.edu.usth.weather;
 
 import android.os.Bundle;
-import android.util.Log;
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.util.Log;
 
 public class WeatherActivity extends AppCompatActivity {
 
@@ -11,12 +17,25 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_weather);
-
-        ForecastFragment firstFragment = new ForecastFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.main, firstFragment).commit();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         Log.i(TAG, "=== APP CREATED ===");
+
+        if (savedInstanceState == null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            ForecastFragment forecastFragment = new ForecastFragment();
+            fragmentTransaction.add(R.id.main, forecastFragment);
+            fragmentTransaction.commit();
+        }
+
     }
 
     public WeatherActivity() {
